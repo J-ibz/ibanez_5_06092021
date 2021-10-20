715a07e05f67c316dbfc088b8733beefda51c280
j-ibz <jeremie.ibanez@gmail.com>
2021-10-20 18:45:25 +0200

    Fetch des données par rapport à l'id

--- front/javascript/product.js
+++ front/javascript/product.js
@@ -1,3 +1,14 @@
 //Récupération de l'id depuis l'url
 let params = new URL(document.location).searchParams;
 let idProduct = params.get("id");
+
+//Fetch des données par rapport à l'id récupéré dans l'url du produit
+const fetchProductId = async function () {
+    await fetch(`http://localhost:3000/api/products/${idProduct}`)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            produits = data;
+        });
+};
