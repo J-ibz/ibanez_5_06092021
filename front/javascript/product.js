d111ef4677be1ef97b5ace18b2113bc2e4f063be
j-ibz <jeremie.ibanez@gmail.com>
2021-10-20 18:45:54 +0200

    Affichage du produit

--- front/javascript/product.js
+++ front/javascript/product.js
@@ -12,3 +12,17 @@ const fetchProductId = async function () {
             produits = data;
         });
 };
+
+// Affichage du produit
+const afficherLeProduit = async function () {
+    await fetchProductId();
+    let choixColor = document.querySelector("#colors");
+    document.querySelector(".item__img").innerHTML = `<img src="${produits.imageUrl}" alt="${produits.altTxt}">`;
+    document.getElementById("title").textContent = produits.name;
+    document.getElementById("price").textContent = produits.price;
+    document.getElementById("description").textContent = produits.description;
+    produits.colors.forEach((option) => {
+        choixColor.innerHTML += `<option value="${option}">${option}</option>`;
+    });
+};
+afficherLeProduit();
