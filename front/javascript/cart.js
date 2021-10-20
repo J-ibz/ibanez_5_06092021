44364aed7197f9e36a0974625176265eff607e4b
j-ibz <jeremie.ibanez@gmail.com>
2021-10-20 18:50:00 +0200

    Affichage des éléments du panier

---
+++ front/javascript/cart.js
@@ -0,0 +1,39 @@
+//Récupération des infos stocké dans le local storage
+let panier = JSON.parse(localStorage.getItem("produit"));
+
+//Affichage des éléments du panier
+function afficherPanier() {
+    //Si panier vide
+    if (panier === null || panier.length == 0) {
+        document.querySelector("#cartAndFormContainer > h1").textContent += " est vide";
+    }
+    // si element dans panier
+    else {
+        for (i = 0; i < panier.length; i++) {
+            document.querySelector("#cart__items").innerHTML += `
+    <article class="cart__item" data-id="${panier[i][0].idProduct}">
+        <div class="cart__item__img">
+            <img src="${panier[i][0].image}" alt="${panier[i][0].imageAlt}" />
+        </div>
+        <div class="cart__item__content">
+            <div class="cart__item__content__titlePrice">
+                <h2>${panier[i][0].name}</h2>
+                <p>${panier[i][0].price}</p>
+                <p>Couleur: ${panier[i][0].choixOpt}</p>
+            </div>
+            <div class="cart__item__content__settings">
+                <div class="cart__item__content__settings__quantity">
+                    <p>Qté :</p>
+                    <input index="${[i]}" onchange="getNewQty(this)" id="cartQty" type="number" class="itemQuantity" name="itemQuantity" min="0" max="100" value="${panier[i][0].qty}" />
+                </div>
+                <div class="cart__item__content__settings__delete">
+                    <p index="${[i]}" onclick="supprimerArticle(this)" class="deleteItem">Supprimer</p>
+                </div>
+            </div>
+        </div>
+    </article>
+    `;
+        }
+    }
+}
+afficherPanier();
